6b29e1294b78071a0e8e974769b1f6d38c10158c
Matheus Sollitto <matheus.sollitto@muralis.com.br>
2024-04-25 19:47:09 -0300

    initial commit

---
+++ 05_FirstStepsInjavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/teste/MeuObjeto.java
@@ -0,0 +1,28 @@
+package br.com.teste;
+
+public class MeuObjeto {
+
+    private String valor1;
+    private String valor2;
+
+    public MeuObjeto(String valor1, String valor2) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+    public String getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(String valor1) {
+        this.valor1 = valor1;
+    }
+
+    public String getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(String valor2) {
+        this.valor2 = valor2;
+    }
+}
